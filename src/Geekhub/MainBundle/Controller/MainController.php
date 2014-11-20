0c01c0f83476ae071fe87c86c80c50cb7fc0c20d
intensity <intensity.web@gmail.com>
2014-11-20 12:37:35 +0200

    Twig features, Doctrine(entities,relations) & simple Fixtures

--- src/Geekhub/MainBundle/Controller/MainController.php
+++ src/Geekhub/MainBundle/Controller/MainController.php
@@ -14,12 +14,42 @@ class MainController extends Controller
     public function indexAction($name)
     {
         $twigArray = [
-            'key'  => 'value',
+            'key' => 'value',
             'test' => 'val',
             'geek' => 'team',
         ];
         $serviceResult = $this->get('my_simple_service')->returnServiceName();
 
         return $this->render('GeekhubMainBundle:Main:index.html.twig', ['name' => $name, 'serviceResult' => $serviceResult, 'twigArray' => $twigArray]);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showPostsAction()
+    {
+        $posts = $this->getDoctrine()->getRepository('GeekhubMainBundle:Post')->findAll();
+
+        return $this->render('GeekhubMainBundle:Main:showPosts.html.twig', ['posts' => $posts]);
+    }
+
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showPostAction($id)
+    {
+        $post = $this->getDoctrine()->getRepository('GeekhubMainBundle:Post')->find($id);
+
+        return $this->render('GeekhubMainBundle:Main:showPost.html.twig', ['post' => $post]);
+    }
+
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function testTwigAction()
+    {
+        return $this->render('GeekhubMainBundle:Main:testTwig.html.twig');
     }
 }
---
+++ src/Geekhub/MainBundle/Entity/Post.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Geekhub\MainBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post Entity
+ *
+ * @ORM\Table(name="geekhub_posts")
+ * @ORM\Entity
+ */
+class Post
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(name="content", type="string", length=255)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(name="date", type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Comment", mappedBy="post", cascade={"persist"})
+     */
+    private $comments;
+
+    public function __construct()
+    {
+        $this->date = new \DateTime();
+        $this->comments = new ArrayCollection();
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param $title
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param $content
+     * @return $this
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param \DateTime $date
+     * @return $this
+     */
+    public function setDate(\DateTime $date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param Comment $comment
+     * @return $this
+     */
+    public function setComments(Comment $comment)
+    {
+        $comment->setPost($this);
+        $this->comments[] = $comment;
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getComments()
+    {
+        return $this->comments;
+    }
+}
